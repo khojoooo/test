bfacfb68e853dc0307cd41850f47c56817d608f8
minho <clilc@naver.com>
2020-03-25 03:06:28 +0900

    header

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Header, Segment} from 'semantic-ui-react';
 import { Link } from '../routes';
+import './index.css';
 
 export default () => {
   return (
-  <Segment inverted>
-    <Header as='h1' inverted color='grey' textAlign='center' size='huge' style={{ marginTop: '50px', height:'100px'}}>
-      <font size="20" >
+  <Segment inverted color='blue'>
+    <Header as='h1'  textAlign='center' size='huge' style={{ marginTop: '18px', height:'50px'}}>
+      <font size="6" >
         <Link route="/">
-          <a className="item">
-            Safe registered mail
+          <a className="item"  style={{color:'white'}}>
+            고객님께 안전을 보장하겠습니다
           </a>
         </Link>
       </font>
